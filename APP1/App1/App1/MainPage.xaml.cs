5080c25c7a18d537f2dca48a13f11fec927b91b7
Palette25 <30751070+Palette25@users.noreply.github.com>
2018-03-19 23:24:15 +0800

    Homework1 update

---
+++ APP1/App1/App1/MainPage.xaml.cs
@@ -0,0 +1,221 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace App1
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            var view = ApplicationView.GetForCurrentView();
+            view.TitleBar.BackgroundColor = Colors.AliceBlue;
+            view.Title = "Mytodos";
+            this.InitializeComponent();
+            on_Load_Check_Store();
+            on_Load_Check_Deal();
+        }
+        public void box_Checked(object sender, RoutedEventArgs e)
+        {
+            CheckBox box = (CheckBox)sender;
+            string name = box.Name;
+            DealItemCollection.name_collection.Add(name);
+            char num = name[name.Length-1];
+            Line target = (Line)this.FindName("line" + num);
+            target.Visibility = Visibility.Visible;
+        }
+
+        public void box_Unchecked(object sender, RoutedEventArgs e)
+        {
+            CheckBox box = (CheckBox)sender;
+            string name = box.Name;
+            DealItemCollection.name_collection.Remove(name);
+            Debug.WriteLine(DealItemCollection.name_collection.Count);
+            char num = name[name.Length - 1];
+            Line target = (Line)this.FindName("line" + num);
+            target.Visibility = Visibility.Collapsed;
+        }
+
+        private void btn1_click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(NextPage), "");
+            app_btn.Visibility = Visibility.Collapsed;
+        }
+        
+        private void on_Load_Check_Store()
+        {
+            if (ItemStore.store == null) return;
+            for(int i=0;i<ItemStore.store.Count;i++)
+            {
+                Item item = (Item)ItemStore.store[i];
+                item.AddToPage(this);
+            }
+        }
+
+        private void on_Load_Check_Deal()
+        {
+            if (DealItemCollection.name_collection == null) return;
+            foreach(string name in DealItemCollection.name_collection)
+            {
+                CheckBox deal_box = (CheckBox)this.FindName(name);
+                deal_box.IsChecked = true;
+                string line_name = "line" + name[name.Length - 1];
+                Line deal_line = (Line)this.FindName(line_name);
+                deal_line.Visibility = Visibility.Visible;
+            }
+        }
+
+
+        private void delete_item(object sender, RoutedEventArgs e)
+        {
+            MenuFlyoutItem Mitem = (MenuFlyoutItem)sender;
+            int id = int.Parse(Mitem.Name);
+            for (int i = 0; i < ItemStore.store.Count; i++)
+            {
+                Item item = (Item)ItemStore.store[i];
+                if (item.id == id)
+                {
+                    ItemStore.store.Remove(item);
+                    ItemStore.num -= 1;
+                }
+            }
+            Frame frame = Window.Current.Content as Frame;
+            frame.Navigate(typeof(MainPage));
+        }
+
+        private void edit_item(object sender, RoutedEventArgs e)
+        {
+            MenuFlyoutItem Mitem = (MenuFlyoutItem)sender;
+            int id = int.Parse(Mitem.Name);
+            Item mess = null;
+            for(int i = 0; i < ItemStore.store.Count; i++)
+            {
+                Item item = (Item)ItemStore.store[i];
+                if(item.id == id)
+                {
+                    mess = new Item(item.text, item.content, item.source, item.date, item.id);
+                }
+            }
+            Frame frame = Window.Current.Content as Frame;
+            frame.Navigate(typeof(NextPage), mess);
+        }
+
+
+        public class Item
+        {
+            public string text;
+            public string content;
+            public ImageSource source;
+            public DateTimeOffset date;
+            public int id;
+            public Item(string text, string content, ImageSource source, DateTimeOffset date, int id)
+            {
+                this.text = text;
+                this.content = content;
+                this.source = source;
+                this.date = date;
+                this.id = id;
+            }
+            public void AddToPage(MainPage page)
+            {
+                //New CheckBox
+                CheckBox new_box = new CheckBox()
+                {
+                    Name = "box" + id,
+                    Margin = new Thickness(10, 20, 0, 0)
+                };
+                new_box.Checked += page.box_Checked;
+                new_box.Unchecked += page.box_Unchecked;
+                page.main.Children.Add(new_box);
+                Grid.SetColumn(new_box, 0);
+                Grid.SetRow(new_box, id);
+                //New Image
+                Image new_img = new Image
+                {
+                    Source = source,
+                    Height = 100,
+                    Width = 120
+                };
+                Grid.SetColumn(new_img, 1);
+                Grid.SetRow(new_img, id);
+                page.main.Children.Add(new_img);
+                //New Text
+                TextBlock new_text = new TextBlock()
+                {
+                    Text = text,
+                    Margin = new Thickness(20, 30, 169, 28),
+                    FontSize = 20
+                };
+                Grid.SetColumn(new_text, 2);
+                Grid.SetRow(new_text, id);
+                page.main.Children.Add(new_text);
+                //New Line
+                Line new_line = new Line()
+                {
+                    X1 = 1,
+                    Stretch = Stretch.Fill,
+                    Height = 2,
+                    Width = 600,
+                    Stroke = new SolidColorBrush(Colors.Black),
+                    StrokeThickness = 2,
+                    Name = "line" + id,
+                    Visibility = Visibility.Collapsed,
+                    Margin = new Thickness(20, 0, 0, 0)
+                };
+                Grid.SetColumn(new_line, 2);
+                Grid.SetRow(new_line, id);
+                page.main.Children.Add(new_line);
+                //New btn
+                AppBarButton new_btn = new AppBarButton()
+                {
+                    Margin = new Thickness(0, 0, 20, 0),
+                    Icon = new SymbolIcon(Symbol.Setting),
+                    IsCompact = true,
+                    VerticalAlignment = VerticalAlignment.Center,
+                    HorizontalAlignment = HorizontalAlignment.Right
+                };
+                MenuFlyout new_menu = new MenuFlyout();
+                MenuFlyoutItem item1 = new MenuFlyoutItem()
+                {
+                    Text = "Edit",
+                    Name = id.ToString()
+                };
+                item1.Click += page.edit_item;
+                MenuFlyoutItem item2 = new MenuFlyoutItem
+                {
+                    Text = "Delete",
+                    Name = id.ToString()
+                };
+                item2.Click += page.delete_item;
+                new_menu.Items.Add(item1);
+                new_menu.Items.Add(item2);
+                new_btn.Flyout = new_menu;
+                Grid.SetColumn(new_btn, 3);
+                Grid.SetRow(new_btn, id);
+                page.main.Children.Add(new_btn);
+            }
+
+        }
+
+    }
+}
---
+++ APP1/App1/App1/NextPage.xaml.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using Microsoft.Win32;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.Storage;
+using Windows.UI.Xaml.Media.Imaging;
+using static App1.MainPage;
+
+// https://go.microsoft.com/fwlink/?LinkId=234238 上介绍了“空白页”项模板
+
+namespace App1
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class NextPage : Page
+    {
+        public int num = 0;
+        public NextPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void on_create(object sender, RoutedEventArgs e)
+        {
+            Button btn = (Button)sender;
+            bool mode = false; ;
+            if ((string)btn.Content=="Save")
+            {
+                mode = true;
+            }
+            String error_message = "";
+            if (title.Text == "")
+            {
+                error_message += "Title cannot be empty!\n";
+            }
+            if (content.Text == "")
+            {
+                error_message += "Content cannot be empty!\n";
+            }
+            if(Smaller(date))
+            {
+                error_message += "The date has already passed, please change another date!\n";
+            }
+            if(error_message == "")
+            {
+                if (mode) error_message = "Save successfully!";
+                else error_message = "Create successfully!";
+            }
+            var cd = new ContentDialog()
+            {
+                Content = error_message,
+                PrimaryButtonText = "OK",
+            };
+            cd.PrimaryButtonClick += (s_, e_) => { };
+            await cd.ShowAsync();
+            if (error_message == "Create successfully!")
+            {
+                Item new_item = new Item(title.Text, content.Text, todo_img.Source, date.Date, ItemStore.num);
+                ItemStore.num++;
+                ItemStore.store.Add(new_item);
+                Frame.GoBack();
+            }else if(error_message == "Save successfully!")
+            {
+                for(int i = 0; i < ItemStore.store.Count; i++)
+                {
+                    Item item = (Item)ItemStore.store[i];
+                    if (item.id == num)
+                    {
+                        item.text = title.Text;
+                        item.content = content.Text;
+                        item.source = todo_img.Source;
+                        item.date = date.Date;
+                    }
+                }
+                Frame.GoBack();
+            }
+        }
+
+        private void on_cancel(object sender, RoutedEventArgs e)
+        {
+            title.Text = "";
+            content.Text = "";
+            date.Date = DateTime.Now;
+        }
+
+        private bool Smaller(DatePicker date)
+        {
+            int res = date.Date.CompareTo(DateTime.Now);
+            if (res == -1)
+            {
+                if (date.Date.Year==DateTime.Now.Year&&date.Date.Month==DateTime.Now.Month&&date.Date.Day==DateTime.Now.Day)
+                    return false;
+                else return true;
+            }
+            else return false;
+        }
+
+        private void btn2_click(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(MainPage), "");
+        }
+
+        private async void img_click(object sender, RoutedEventArgs e)
+        {
+            var srcImage = new BitmapImage();
+            FileOpenPicker file = new FileOpenPicker();
+            file.FileTypeFilter.Add(".jpg");
+            file.FileTypeFilter.Add(".png");
+            file.FileTypeFilter.Add(".jpeg");
+            Windows.Storage.StorageFile result = await file.PickSingleFileAsync();
+            if (result != null)
+            {
+                using(IRandomAccessStream stream = await result.OpenAsync(FileAccessMode.Read))
+                {
+                    await srcImage.SetSourceAsync(stream);
+                    todo_img.Source = srcImage;
+                }
+            }
+        }
+
+        private void Slider_ValueChanged(object sender, RangeBaseValueChangedEventArgs e)
+        {
+            Slider sli = sender as Slider;
+            if(sli != null)
+            {
+                todo_img.Height = 200 + sli.Value*0.3;
+                todo_img.Width = 200 + sli.Value*0.3;
+            }
+        }
+        
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            if(e.Parameter != "")
+            {
+                Item old = (Item)e.Parameter;
+                create.Content = "Save";
+                title.Text = old.text;
+                content.Text = old.content;
+                date.Date = old.date;
+                todo_img.Source = old.source;
+                num = old.id;
+            }
+        }
+    }
+   
+}
